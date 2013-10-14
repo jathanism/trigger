ace83d4ddddee3cd0790f3b0af57719aed2d838b
jathanism <jathan@gmail.com>
2013-10-14 15:15:01 -0700

    Fix a typo in logger, add explicit vendor support for now

---
+++ configfetcher.py
@@ -0,0 +1,43 @@
+
+import sys
+from twisted.python import log
+log.startLogging(sys.stdout, setStdout=False)
+from trigger.cmds import Commando
+import xml.etree.cElementTree as ET
+
+class ConfigFetcher(Commando):
+    vendors = ['paloalto', 'juniper', 'aruba', 'netscreen']
+    config_map = {
+        'juniper': 'show configuration',
+        'paloalto': 'show config running',
+        'aruba': 'show configuration',
+    }
+
+    def from_base(self, results, device):
+        """Call store_results without calling the default of map_results"""
+        log.msg('Received %r from %s' % (results, device))
+        self.store_results(device, results)
+
+    def _ioslike(self):
+        return ['show running-config']
+
+    def to_base(self, device, commands=None, extra=None):
+        commands = commands or self.commands
+        if device.is_ioslike():
+            return self._ioslike()
+        log.msg('Sending %r to %s' % (commands, device))
+        return commands
+
+    def to_juniper(self, device, commands=None, extra=None):
+        if self.force_cli:
+            return ['show configuration']
+        elem = ET.Element('get-configuration', database='committed',
+                          inherit='inherit')
+        self.commands = [elem]
+        return self.commands
+
+    def to_aruba(self, device, commands=None, extra=None):
+        return ['show config running']
+
+    def to_netscreen(self, device, commands=None, extra=None):
+        return ['get config']
